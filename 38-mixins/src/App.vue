8b2b535fbc54a4e47863d4eed85753ea995dc304
TanzimIbthesam <35595427+TanzimIbthesam@users.noreply.github.com>
2019-10-12 00:16:06 +0600

    Add files via upload

---
+++ 38-mixins/src/App.vue
@@ -0,0 +1,49 @@
+
+<template>
+<div>
+<show-blog></show-blog>
+<list-blog></list-blog>
+</div>
+ 
+
+ 
+</template>
+ <script>
+
+
+import showBlog from './components/showBlog.vue';
+import AddBlog from './components/AddBlog.vue'; 
+import listBlog from './components/listBlog.vue'; 
+  export default{
+    components:{
+      
+      'show-blog':showBlog,
+      'add-blog':AddBlog,
+      'list-blog':listBlog
+      
+    },
+    data(){
+      return{
+      //  title:'I am a dynamic slot title'
+         
+        
+      }
+    },
+   methods: {
+        
+    }
+   
+   
+
+    }
+  
+  </script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
